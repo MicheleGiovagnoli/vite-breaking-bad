a3867c54dfe44ea737f34350a437899503a91b31
michele <michele.giovagnoli1@gmail.com>
2023-03-29 16:50:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+import { store } from './store.js';
+
+import axios from 'axios';
+
+export default {
+  components: {
+    AppHeader,
+  },
+  data() {
+    return store
+  },
+  created() {
+    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Alien')
+      .then((response) => {
+        console.log(response.data.archetype);
+      })
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style lang="scss">
+@use './styles/general.scss';
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+//Importo la variabile 'store' dal file store.js
+import { store } from '../store.js';
+
+export default {
+    nome: "AppHeader",
+    data() {
+        return {
+            store
+        }
+    },
+};
+</script>
+
+<template>
+    <div>prova</div>
+</template>
+
+<style lang="scss"></style>
